33dc44fc58902df190c378e06a236308c5aca00d
azad-YM <02ym.azad@gmail.com>
2025-05-17 22:59:34 +0300

    Task: get moods from api

--- resources/js/lib/api.ts
+++ resources/js/lib/api.ts
@@ -3,3 +3,8 @@ export const getPages = async () => {
   const res = await fetch('/api/pages')
   return await res.json()
 }
+
+export const getMoods = async () => {
+  const res = await fetch('/api/moods')
+  return await res.json()
+}
--- resources/js/sections/trending-pages/trending-pages.hook.ts
+++ resources/js/sections/trending-pages/trending-pages.hook.ts
@@ -1,12 +1,14 @@
-import { getPages } from "@/lib/api"
+import { getMoods, getPages } from "@/lib/api"
 import { moods } from "@/lib/data"
 import { useQuery } from "@tanstack/react-query"
 
 export const useTrendingPage = () => {
   const query = useQuery({ queryKey: ['pages'], queryFn: getPages })
+  const queryMoods = useQuery({ queryKey: ['moods'], queryFn: getMoods })
+
   return {
     pages: query.data?.data ?? [],
     isPending: query.isPending,
-    moods
+    moods: queryMoods.data ?? []
   }
 }
